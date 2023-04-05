eb99ab7216d7094024befbfe390f2e3d01010b8c
thiagorf <trfarciroli@gmail.com>
2023-04-05 17:09:23 -0300

    clean auth service

--- packages/auth/src/app.module.ts
+++ packages/auth/src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, PrismaModule, HttpModule],
+  imports: [ConfigModule.forRoot(), UserModule,  HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
