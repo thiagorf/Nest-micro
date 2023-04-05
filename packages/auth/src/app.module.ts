57285af8d5a215160694e3b77cabd08e56426756
thiagorf <trfarciroli@gmail.com>
2023-04-05 17:45:00 -0300

    cleanup project

--- packages/auth/src/app.module.ts
+++ packages/auth/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule,  HttpModule],
+  imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
