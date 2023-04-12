a2b22464b05777e4338d970b300122eab7c42ef6
thiagorf <trfarciroli@gmail.com>
2023-04-12 16:21:57 -0300

    create signup and authorization callback api routes

--- packages/auth/src/app.controller.ts
+++ packages/auth/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller,  Post, } from '@nestjs/common';
+import { Body, Controller, Post, Query, Get, Headers } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -9,5 +9,20 @@ export class AppController {
   @Post('signup')
   async signUp(@Body() createUserDto: CreateUserDto) {
     return await this.appService.signUp(createUserDto);
+  }
+
+  @Get('callback')
+  async callback(@Query('code') code: string) {
+    // http://localhost:8080/realms/ordering/protocol/openid-connect/auth?response_type=code&client_id=ordering&redirect_uri=http://localhost:3002/auth/callback&scope=openid
+    // http://localhost:3002/auth/callback?session_state=32e8d6de-4568-47d1-857e-b928e075401e&code=3f09ef04-d258-4cbe-af1c-29275c4a7aea.32e8d6de-4568-47d1-857e-b928e075401e.1cb06168-bfca-4deb-9567-4b21bd249493
+    // http://localhost:8080/realms/ordering/protocol/openid-connect/logout
+    return await this.appService.callback(code);
+  }
+
+  @Get("whoiam")
+  async whoIAm(@Headers("Authorization") bearerToken: string) {
+    const [, jwt] = bearerToken.split(" ");
+
+    return await this.appService.whoIAm(jwt);
   }
 }
--- packages/auth/src/app.service.ts
+++ packages/auth/src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { lastValueFrom, map } from 'rxjs';
 import { CreateUserDto } from './dto/create-user.dto';
-import { LoginDto } from './dto/login.dto';
 import { OpenIdToken } from './interfaces/openid-token.interface';
 
 @Injectable()
@@ -13,15 +12,46 @@ export class AppService {
     private configService: ConfigService,
   ) {}
 
-  async signIn(loginDto: LoginDto) {}
+  async callback(code: string) {
+    const token = await lastValueFrom(
+      this.httpService
+        .post<OpenIdToken>(
+          this.configService.get('KEYCLOAK_ACCESS_TOKEN'),
+          {
+            code,
+            grant_type: 'authorization_code',
+            redirect_uri: 'http://localhost:3002/auth/callback',
+            client_id: this.configService.get('KEYCLOAK_CLIENT_ID'),
+            cliebt_secret: this.configService.get('KEYCLOAK_CLIENT_SECRET'),
+          },
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          },
+        )
+        .pipe(map((r) => r.data)),
+    );
+
+    return token;
+  }
+
   async signUp(createUserDto: CreateUserDto) {
     const token = await lastValueFrom(
       this.httpService
-        .post(this.configService.get('KEYCLOAK_ACCESS_TOKEN'), {
-          grant_type: 'client_credentials',
-          client_id: this.configService.get('KEYCLOAK_CLIENT_ID'),
-          client_secret: this.configService.get('KEYCLOAK_CLIENT_SECRET'),
-        })
+        .post(
+          this.configService.get('KEYCLOAK_ACCESS_TOKEN'),
+          {
+            grant_type: 'client_credentials',
+            client_id: this.configService.get('KEYCLOAK_CLIENT_ID'),
+            client_secret: this.configService.get('KEYCLOAK_CLIENT_SECRET'),
+          },
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          },
+        )
         .pipe(map((resp) => resp.data)),
     );
 
@@ -46,11 +76,30 @@ export class AppService {
           {
             headers: {
               Authorization: `Bearer ${token.access_token}`,
+              'Content-Type': 'application/json',
             },
           },
         )
         .pipe(map((r) => r.data)),
     );
-        return user;
+    return user;
+  }
+
+  async whoIAm(jwt: string) {
+    const me = await lastValueFrom(
+      this.httpService
+        .get(
+          'http://localhost:8080/admin/realms/ordering/protocol/openid-connect/userinfo',
+          {
+            headers: {
+              Authorization: `Bearer ${jwt}`,
+            },
+          },
+        )
+        .pipe(map((r) => r.data)),
+    );
+
+    return me;
+    
   }
 }
